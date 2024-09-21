1e018ec3648fce3021971443e5859bcd858978bc
iagoborelli <iago.borelli1304@gmail.com>
2024-09-21 10:31:00 -0300

    view da tela de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(){
+        return view("login");
+    }
+
+    public function logout(){
+        echo "logout";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
+// auth routes
+
+Route::get("/login", [AuthController::class, "login"]);
+Route::get("/logout", [AuthController::class, "logout"]);
