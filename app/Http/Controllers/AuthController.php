e47d3bf9680d225797fc9c8a9a01caf317ab7d9b
iagoborelli <iago.borelli1304@gmail.com>
2024-09-22 16:43:41 -0300

    personalização das mensagens de erro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,15 +16,22 @@ class AuthController extends Controller
         // form validation
         $request->validate(
             [
-                'text_username' => 'required',
-                'text_password' => 'required'
-            ]    
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            [
+                'text_username.required' =>  'O username é obrigatório.',
+                'text_username.email' =>  'Username deve ser um email válido.',
+                'text_password.required' =>  'O password é obrigatório.',
+                'text_password.min' =>  'O password deve ter pelo menos :min caracteres.',
+                'text_password.max' =>  'O password deve ter no máximo :max caracteres.'
+            ]
         );
 
         // get user input
         $username = $request->input('text_username');
         $password = $request->input('text_password');
-        
+
         echo 'OK!';
     }
 
