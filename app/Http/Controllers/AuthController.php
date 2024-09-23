fe444ec9ce7cdbe00aa8b5736d4da8ad7e38728d
iagoborelli <iago.borelli1304@gmail.com>
2024-09-23 19:47:20 -0300

    criação da database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -32,7 +33,14 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        echo 'OK!';
+        try{
+            DB::connection()->getPdo();
+            echo "Connection is ok!";
+        } catch (\PDOException $e){
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+        echo "fim!";
     }
 
     public function logout()
---
+++ database/migrations/2024_09_23_223506_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('username', 50)->nullable();
+            $table->string('password', 200)->nullable();
+            $table->dateTime('last_login')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
