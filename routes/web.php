0664a2d9d5fd2fe90c9efe399f7454dc65577c7f
iagoborelli <iago.borelli1304@gmail.com>
2024-09-24 19:45:13 -0300

    definição de rotas da view home

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::middleware([CheckIsNotLogged::class])->group(function () {
 
 
 Route::middleware([CheckIsLogged::class])->group(function () {
-    Route::get("/", [MainController::class, "index"]);
-    Route::get("/newNote", [MainController::class, "newNote"]);
-    Route::get("/logout", [AuthController::class, "logout"]);
+    Route::get("/", [MainController::class, "index"])->name("home");
+    Route::get("/newNote", [MainController::class, "newNote"])->name("new");
+    Route::get("/logout", [AuthController::class, "logout"])->name("logout");
 });
