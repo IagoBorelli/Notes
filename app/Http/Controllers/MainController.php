5afc63bb8004c9268b2383c0da176395acff1234
iagoborelli <iago.borelli1304@gmail.com>
2024-09-24 19:18:42 -0300

    criação de middleware para fazer a verificação se o usuário está ou não logado

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,5 +7,11 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-  
+  public function index(){
+    echo "estou dentro do sistema";
+  }
+
+  public function newNote(){
+    echo "estou a criar uma nova nota";
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsLogged;
 use Illuminate\Support\Facades\Route;
 
 // auth routes
-
 Route::get("/login", [AuthController::class, "login"]);
-Route::post("/loginSubmit", [AuthController::class,"loginSubmit"]);
-Route::get("/logout", [AuthController::class, "logout"]);
+Route::post("/loginSubmit", [AuthController::class, "loginSubmit"]);
+
+Route::middleware([CheckIsLogged::class])->group(function () {
+    Route::get("/", [MainController::class, "index"]);
+    Route::get("/newNote", [MainController::class, "newNote"]);
+    Route::get("/logout", [AuthController::class, "logout"]);
+});
