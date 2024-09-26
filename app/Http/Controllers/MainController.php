4680d6812e70e75d8a170be02bc9abf02b64a12a
iagoborelli <iago.borelli1304@gmail.com>
2024-09-25 21:14:56 -0300

    Validação de formulário e criação de nova nota

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
@@ -24,18 +25,45 @@ class MainController extends Controller
     return view('new_note');
   }
 
-  public function newNoteSubmit(Request $request){
-    echo 'estou criando uma nova nota';
+  public function newNoteSubmit(Request $request)
+  {
+
+    $request->validate(
+      [
+        'text_title' => 'required|min:3|max:200',
+        'text_note' => 'required|min:3|max:3000'
+      ],
+      [
+        'text_title.required' =>  'O título é obrigatório.',
+        'text_title.min' =>  'O título deve ter pelo menos :min caracteres.',
+        'text_title.max' =>  'O título deve ter no máximo :max caracteres.',
+        'text_note.required' =>  'A nota é obrigatória.',
+        'text_note.min' =>  'A nota deve ter pelo menos :min caracteres.',
+        'text_note.max' =>  'A nota deve ter no máximo :max caracteres.'
+      ]
+    );
+
+    $id = session('user.id');
+
+    $note = new Note();
+    $note->user_id = $id;
+    $note->title = $request->text_title;
+    $note->text = $request->text_note;
+    $note->save();
+
+    return redirect()->route('home');
   }
 
-  public function editNote($id) {
+  public function editNote($id)
+  {
 
     $id = Operations::decryptId($id);
     echo "editing note: $id";
   }
 
-  public function deleteNote($id) {
-    
+  public function deleteNote($id)
+  {
+
     $id = Operations::decryptId($id);
     echo "deleting note: $id";
   }
