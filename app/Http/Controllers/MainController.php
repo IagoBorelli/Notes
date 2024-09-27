22d898093078082ac66bedc184719cb4a99ae721
iagoborelli <iago.borelli1304@gmail.com>
2024-09-26 21:39:16 -0300

    Correção de alguns problemas

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,69 +5,118 @@ namespace App\Http\Controllers;
 use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
+use Illuminate\Cache\RedisStore;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
-  public function index()
-  {
+    public function index()
+    {
+        // load user's notes
+        $id = session('user.id');
+        $notes = User::find($id)->notes()->get()->toArray();
 
-    $id = session('user.id');
-    $notes = User::find($id)->notes()->get()->toArray();
+        // show home view
+        return view('home', ['notes' => $notes]);
+    }
 
-    return view('home', ['notes' => $notes]);
-  }
+    public function newNote()
+    {
+        // show new note view
+        return view('new_note');
+    }
 
-  public function newNote()
-  {
-    return view('new_note');
-  }
+    public function newNoteSubmit(Request $request)
+    {
+        // validate request
+        $request->validate(
+            // rules
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            // error messages
+            [
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve ter pelo menos :min caracteres',
+                'text_title.max' => 'O título deve ter no máximo :max caracteres',
+                'text_note.required' => 'A nota é obrigatória',
+                'text_note.min' => 'A nota deve ter pelo menos :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres'
+            ]
+        );
 
-  public function newNoteSubmit(Request $request)
-  {
+        // get user id
+        $id = session('user.id');
 
-    $request->validate(
-      [
-        'text_title' => 'required|min:3|max:200',
-        'text_note' => 'required|min:3|max:3000'
-      ],
-      [
-        'text_title.required' =>  'O título é obrigatório.',
-        'text_title.min' =>  'O título deve ter pelo menos :min caracteres.',
-        'text_title.max' =>  'O título deve ter no máximo :max caracteres.',
-        'text_note.required' =>  'A nota é obrigatória.',
-        'text_note.min' =>  'A nota deve ter pelo menos :min caracteres.',
-        'text_note.max' =>  'A nota deve ter no máximo :max caracteres.'
-      ]
-    );
+        // create new note
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
 
-    $id = session('user.id');
+        // redirect to home
+        return redirect()->route('home');
+    }
 
-    $note = new Note();
-    $note->user_id = $id;
-    $note->title = $request->text_title;
-    $note->text = $request->text_note;
-    $note->save();
+    public function editNote($id)
+    {
+        $id = Operations::decryptId($id);
+        
+        // load note
+        $note = Note::find($id);
 
-    return redirect()->route('home');
-  }
+        // show edit note view
+        return view('edit_note', ['note' => $note]);
+    }
 
-  public function editNote($id)
-  {
+    public function editNoteSubmit(Request $request)
+    {
+        // validate request
+        $request->validate(
+            // rules
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            // error messages
+            [
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve ter pelo menos :min caracteres',
+                'text_title.max' => 'O título deve ter no máximo :max caracteres',
+                'text_note.required' => 'A nota é obrigatória',
+                'text_note.min' => 'A nota deve ter pelo menos :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres'
+            ]
+        );
 
-    $id = Operations::decryptId($id);
-    
-    $note = Note::find($id);
+        // check if note_id exists
+        if($request->note_id == null){
+            return redirect()->route('home');
+            
+        }
 
-    return view('edit_note', ['note' => $note]);
-  }
+        // decrypt note_id
+        $id = Operations::decryptId($request->note_id);
 
-  public function deleteNote($id)
-  {
+        // load note
+        $note = Note::find($id);
 
-    $id = Operations::decryptId($id);
-    echo "deleting note: $id";
-  }
+        // update note
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        // redirect to home
+        return redirect()->route('home');
+    }
+
+    public function deleteNote($id)
+    {
+        $id = Operations::decryptId($id);
+        echo "I'm deleting note with id = $id";
+    }
 }
